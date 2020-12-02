7769a109922a27a68256659a31500cd8cbf7cc11
sebas <seb@tageule>
2020-12-03 00:03:26 +0100

    MAJ-2 Ajout action afficher toutes les pièces + création vue correspondante Sebastien 03/12/2020 00:03

--- StockHouse/Controllers/PieceController.cs
+++ StockHouse/Controllers/PieceController.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using StockHouse.Models;
+using StockHouse.Requetes;
 
 namespace StockHouse.Controllers
 {
     public class PieceController : Controller
     {
+        private readonly Requete<Piece> _requetes = new Requete<Piece>();
         // GET: Piece
         [Route("Piece/Index")]
         public ActionResult Index()
         {
             return View();
+        }
+        [Route("Piece/Toutes-les-pieces")]
+        public async Task<ActionResult> ToutesLesPieces()
+        {
+            List<Piece> listPieces = (List<Piece>) await _requetes.GetAllAsync();
+
+            return View(listPieces);
         }
     }
 }
