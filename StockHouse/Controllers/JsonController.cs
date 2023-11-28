aaf7a54c5adb58d6b0c13f66b68181d476a8a51f
Sébastien Damsin <sebastien.damsin@auxipress.be>
2023-11-28 21:28:38 +0100

    Change serializer to NewtonSoft

--- StockHouse/Controllers/JsonController.cs
+++ StockHouse/Controllers/JsonController.cs
@@ -1,46 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
-using System.Web.Script.Serialization;
+using Newtonsoft.Json;
 using StockHouse.Models;
 using StockHouse.Requetes;
 
 namespace StockHouse.Controllers
 {
-    public class JsonController : Controller
+  public class JsonController : Controller
+  {
+    private readonly Requete<Produit> _requetesProduit = new Requete<Produit>();
+
+    // GET: Json
+    //public ActionResult Index()
+    //{
+    //    return View();
+    //}
+
+    [HttpGet]
+    [Route("Json/GetAllProduct")]
+    public async Task<string> GetAllProduct()
     {
-        private readonly Requete<Produit> _requetesProduit = new Requete<Produit>();
+      var resList = await _requetesProduit.GetAllAsync();
 
-        // GET: Json
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
+      var json = JsonConvert.SerializeObject(resList);
 
-        [HttpGet]
-        [Route("Json/GetAllProduct")]
-        public async Task<string> GetAllProduct()
-        {
-            var resList = await _requetesProduit.GetAllAsync();
-
-            var json = new JavaScriptSerializer().Serialize(resList);
-
-            return json;
-        }
-
-        [HttpGet]
-        [Route("Json/GetProductById/{id}")]
-        public async Task<string> GetProductById(int id)
-        {
-            var resProduit = await _requetesProduit.GetByIdAsync(id);
-
-            var json = new JavaScriptSerializer().Serialize(resProduit);
-
-            return json;
-        }
-
+      return json;
     }
+
+    [HttpGet]
+    [Route("Json/GetProductById/{id}")]
+    public async Task<string> GetProductById(int id)
+    {
+      var resProduit = await _requetesProduit.GetByIdAsync(id);
+
+      var json = JsonConvert.SerializeObject(resProduit);
+
+      return json;
+    }
+
+  }
 }
